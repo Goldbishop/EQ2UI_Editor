d8afbe922db37bf3fb078ed69675a1816c9fc1ad
John <3235535+Goldbishop@users.noreply.github.com>
2020-06-24 21:33:53 -0500

    Refactored Form1 to MainForm

--- src/EQ2UI_Editor.UI/Program.cs
+++ src/EQ2UI_Editor.UI/Program.cs
@@ -13,7 +13,7 @@ namespace EQ2UI_Editor.UI {
 		static void Main ( ) {
 			Application.EnableVisualStyles ();
 			Application.SetCompatibleTextRenderingDefault ( false );
-			Application.Run ( new Form1 () );
+			Application.Run ( new MainForm () );
 		}
 	}
 }
