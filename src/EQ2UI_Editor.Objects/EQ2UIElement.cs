eab396188fc0e8c740a14e0b18b85e9e958199d1
John <3235535+Goldbishop@users.noreply.github.com>
2020-07-06 06:41:31 -0500

    * Added base object EQ2UIElement
    * Added Page Element object

---
+++ src/EQ2UI_Editor.Objects/EQ2UIElement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EQ2UI_Editor.Objects {
+	/// <summary>
+	/// Class to describe an individual XML Element; Attributes and Child Elements
+	/// </summary>
+	public abstract class EQ2UIElement {
+		private Dictionary<String , Object> _attributes;
+		private List<EQ2UIElement> _children;
+
+		public EQ2UIElement ( String name ) : this () {
+			this.ElementName = name;
+		}
+		private EQ2UIElement ( ) {
+			// Initialize Variables
+			this._attributes = new Dictionary<String , Object> ();
+			this._children = new List<EQ2UIElement> ();
+		}
+
+		#region Properties
+		/// <summary>
+		/// Returns the Name of the Element.  Name is defined upon constructor initialization
+		/// </summary>
+		public String ElementName { get; }
+
+		#endregion
+
+		#region Methods
+		protected Object GetAttribute ( String key ) {
+			return this.GetAttribute<Object> ( key );
+		}
+		protected T GetAttribute<T> ( String key ) {
+			return ( T ) this._attributes [ key ];
+		}
+		/// <summary>
+		/// Creates or Sets the <paramref name="value"/> of the <paramref name="key"/>
+		/// </summary>
+		/// <param name="key">String value; Represents the unique key to store</param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		protected Boolean SetAttribute<T> ( String key , T value ) {
+			Boolean result = true;
+
+			try {
+				if ( this._attributes.ContainsKey ( key ) ) {
+					this._attributes [ key ] = value;
+				} else {
+					this._attributes.Add ( key , value );
+				}
+			} catch ( Exception ) {
+				result = false;
+			}
+
+			return result;
+		}
+
+		#endregion
+	}
+}
--- src/EQ2UI_Editor.Objects/PageElement.cs
+++ src/EQ2UI_Editor.Objects/PageElement.cs
@@ -1,9 +1,62 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Windows;
+using System.Xml.Serialization;
 
 namespace EQ2UI_Editor.Objects {
 	[XmlRoot ( ElementName = "Page" , Namespace = "" , IsNullable = false )]
-	public class PageElement {
+	public class EQ2UIPage : EQ2UIElement {
+		public EQ2UIPage ( ) : base ( "Page" ) { }
+
+		#region Attributes
+		[XmlAttribute ( AttributeName = "AbsorbsInput" , Namespace = "" , DataType = "boolean" )]
+		public Boolean AbsorbsInput { get; set; } = false;
+		[XmlAttribute ( AttributeName = "Cursor" , Namespace = "" , DataType = "boolean" )]
+		public String Cursor { get; set; }
+		[XmlAttribute ( AttributeName = "Cursor_Set" , Namespace = "" , DataType = "boolean" )]
+		public String Cursor_Set { get; set; }
+		[XmlAttribute ( AttributeName = "IgnoreTab" , Namespace = "" , DataType = "boolean" )]
+		public Boolean IgnoreTab { get; set; }
+		[XmlAttribute ( AttributeName = "MinimumSize" , Namespace = "" , DataType = "boolean" )]
+		public String MinimumSize { get; set; }
+		[XmlAttribute ( AttributeName = "Name" , Namespace = "" , DataType = "boolean" )]
+		public String Name { get; set; }
+		[XmlAttribute ( AttributeName = "PackSize" , Namespace = "" , DataType = "boolean" )]
+		public String PackSize { get; set; }
+
+		[XmlAttribute ( AttributeName = "ScrollExtent" , Namespace = "" , DataType = "boolean" )]
+		public String ScrollExtent { get; set; }
+		/// <summary>
+		/// TODO: Enable parsing Size class into String; both Set & Get
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Thrown if a comma "," is not within the value</exception>
+		[XmlAttribute ( AttributeName = "Size" , Namespace = "" , DataType = "boolean" )]
+		public String ElementSize {
+			get {
+				var val = this.GetAttribute<Size> ( "Size" );
+
+				return String.Format ( "{0},{1}" , val.Width , val.Height );
+			}
+			set {
+				if ( value.Contains ( "," ) ) {
+					this.SetAttribute<Size> ( "Size" , Size.Parse ( value ) );
+				} else {
+					throw new InvalidOperationException (
+						"Size value is improperly formatted; Format: {Width},{Height}" );
+				}
+
+			}
+		}
+		[XmlAttribute ( AttributeName = "TooltipStyle" , Namespace = "" , DataType = "boolean" )]
+		public String TooltipStyle {
+			get { return this.GetAttribute<String> ( "TooltipStyle" ); }
+			set { this.SetAttribute ( "TooltipStyle" , value ); }
+		}
+		#endregion
+
+		#region Child Elements
 		[XmlArray ( ElementName = "include" , Namespace = "" , IsNullable = false )]
-		public string [] Includes { get; set; }
+		public Object [] Children { get; set; }
+		#endregion
+
 	}
 }
