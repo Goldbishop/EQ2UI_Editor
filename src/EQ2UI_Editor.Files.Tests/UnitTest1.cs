ca29e1be029bb7285f2c3b3ef7aee125c6820bd3
John <3235535+Goldbishop@users.noreply.github.com>
2020-07-10 19:54:29 -0500

    Added Unit Test for the Files Project

---
+++ src/EQ2UI_Editor.Files.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EQ2UI_Editor.Files.Tests {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1 ( ) {
+		}
+	}
+}
